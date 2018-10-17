182382ccd2172c5044629f40b9d719b344f3e5ea
nozomi0966 <nozomotoitech@gmail.com>
2018-10-17 23:08:55 +0900

    add return something

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -51,6 +51,12 @@ func TestNextToken2(t *testing.T) {
 
 	!-/*5;
 	5 < 10 > 5;
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
 	`
 
 	tests := []struct {
@@ -105,6 +111,23 @@ func TestNextToken2(t *testing.T) {
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
@@ -114,7 +137,7 @@ func TestNextToken2(t *testing.T) {
 		tok := l.NextToken()
 		log.Println(tok)
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected = %q, got = %q", i, tt.expectedLiteral, tok.Literal)
+			t.Fatalf("tests[%d] - tokentype wrong. expected = %q, got = %q", i, tt.expectedType, tok.Type)
 		}
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d} - literal wrong. expected=%q, got = %q", i, tt.expectedLiteral, tok.Literal)
