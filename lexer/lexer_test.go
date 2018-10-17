936a5978c4b8094517d40906bdb8adb4bda52311
nozomi0966 <nozomotoitech@gmail.com>
2018-10-17 22:56:12 +0900

    １文字トークン

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -48,6 +48,9 @@ func TestNextToken2(t *testing.T) {
 		x + y;
 	};
 	let result = add(five, ten);
+
+	!-/*5;
+	5 < 10 > 5;
 	`
 
 	tests := []struct {
@@ -89,6 +92,18 @@ func TestNextToken2(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
